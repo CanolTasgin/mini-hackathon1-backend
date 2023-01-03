953308c8329b8e34c809d72f66d39fda142d7ff6
CanolTasgin <ccanol@sabanciuniv.edu>
2023-01-02 23:41:23 -0500

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,16 @@
+# docker build --tag minihackathon1 .
+# docker run -d -p 5000:5000 minihackathon1
+# docker ps
+# docker stop <container id>
+# syntax=docker/dockerfile:1
+
+FROM python:3.8-slim-buster
+
+WORKDIR /python-docker
+
+COPY requirements.txt requirements.txt
+RUN pip3 install -r requirements.txt
+
+COPY . .
+
+CMD [ "python3", "-m" , "flask", "run", "--host=0.0.0.0"]
---
+++ app.py
@@ -0,0 +1,68 @@
+import json
+from flask import Flask, request, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
+  
+@app.route("/data", methods=["GET"])
+def get_data():
+    with open("data.json") as f:
+        data = json.load(f)
+
+    return jsonify(data)
+  
+
+@app.route("/data", methods=["POST"])
+def add_data():
+    with open("data.json") as f:
+        data = json.load(f)
+
+    new_data = request.json
+
+    data.update(new_data)
+
+    with open("data.json", "w") as f:
+        json.dump(data, f)
+
+    return jsonify({"message": "Data added successfully."})
+  
+@app.route("/data/<int:id>", methods=["PUT"])
+def update_data(id):
+    with open("data.json") as f:
+        data = json.load(f)
+
+    rooms = data["rooms"]
+
+    for room in rooms:
+        if room["id"] == id:
+            room.update(request.json)
+            break
+    else:
+        return jsonify({"error": "Room not found."}), 404
+
+    with open("data.json", "w") as f:
+        json.dump(data, f)
+
+    return jsonify({"message": "Data updated successfully."})
+
+@app.route("/data/<int:id>", methods=["DELETE"])
+def delete_data(id):
+    with open("data.json") as f:
+        data = json.load(f)
+
+    rooms = data["rooms"]
+
+    for i, room in enumerate(rooms):
+        if room["id"] == id:
+            del rooms[i]
+            break
+    else:
+        return jsonify({"error": "Room not found."}), 404
+
+    with open("data.json", "w") as f:
+        json.dump(data, f)
+
+    return jsonify({"message": "Data deleted successfully."})
